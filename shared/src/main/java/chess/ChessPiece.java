ed1c1eee46b8e5e7cd49fd99dcf0db42d2fb9c64
Mark Bird <markrbird13@gmail.com>
2024-09-06 14:25:37 -0600

    Implement Bishop moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,9 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +13,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor=pieceColor;
+        this.type=type;
     }
 
     /**
@@ -39,6 +47,7 @@ public class ChessPiece {
         throw new RuntimeException("Not implemented");
     }
 
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -47,6 +56,70 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        if (type == PieceType.BISHOP) {
+            List<ChessMove> moves = new ArrayList<ChessMove>();
+            //moves up to the right
+            int rowIter = myPosition.getRow() + 1;
+            int colIter = myPosition.getColumn() + 1;
+            for (rowIter = myPosition.getRow() + 1; rowIter <= 8; rowIter++) {
+                ChessPosition checkPosition=new ChessPosition(rowIter, colIter);
+                if (board.getPiece(checkPosition) == null) {
+                    ChessMove newMove=new ChessMove(myPosition, checkPosition, null);
+                    moves.add(newMove);
+                } else break;
+                colIter++;
+                if (colIter > 8) break;
+            }
+            //moves down to the left
+            colIter = myPosition.getColumn() - 1;
+            for (rowIter = myPosition.getRow() - 1; rowIter > 0; rowIter--) {
+                ChessPosition checkPosition=new ChessPosition(rowIter, colIter);
+                if (board.getPiece(checkPosition) == null) {
+                    ChessMove newMove=new ChessMove(myPosition, checkPosition, null);
+                    moves.add(newMove);
+                } else break;
+                colIter--;
+                if (colIter <= 0) break;
+            }
+            //moves up to the left
+            colIter = myPosition.getColumn() - 1;
+            for (rowIter = myPosition.getRow() + 1; rowIter <= 8; rowIter++) {
+                ChessPosition checkPosition=new ChessPosition(rowIter, colIter);
+                if (board.getPiece(checkPosition) == null) {
+                    ChessMove newMove=new ChessMove(myPosition, checkPosition, null);
+                    moves.add(newMove);
+                } else break;
+                colIter--;
+                if (colIter <= 0) break;
+            }
+            //moves down to the right
+            colIter = myPosition.getColumn() + 1;
+            for (rowIter = myPosition.getRow() - 1; rowIter > 0; rowIter--) {
+                ChessPosition checkPosition=new ChessPosition(rowIter, colIter);
+                if (board.getPiece(checkPosition) == null) {
+                    ChessMove newMove=new ChessMove(myPosition, checkPosition, null);
+                    moves.add(newMove);
+                } else break;
+                colIter++;
+                if (colIter > 8) break;
+            }
+            return moves;
+            }
+        else {
+            throw new RuntimeException("Not implemented");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that=(ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }

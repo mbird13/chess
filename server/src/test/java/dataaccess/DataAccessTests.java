9b7d1ce7f497ae438d6d2fe09b415505af09989e
Mark Bird <markrbird13@gmail.com>
2024-10-25 11:54:11 -0600

    improve clear database test

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import model.AuthData;
 import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.Assertions;
@@ -9,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.stream.Stream;
 
@@ -25,11 +27,71 @@ public class DataAccessTests {
   void clear(DataAccess database) {
     database.createUser("name", "password", "email");
     Assertions.assertEquals(new UserData("name", "password", "email"), database.getUser("name"));
+    database.createUser("2", "2", "@");
+    database.createGame("game");
+    database.createGame("2");
+    database.createGame("pp");
+    database.createAuth(new AuthData("token", "name"));
+    database.createAuth(new AuthData("2", "2"));
 
-    database.clear();
+    Assertions.assertDoesNotThrow(database::clear);
 
     Assertions.assertNull(database.getUser("name"));
     Assertions.assertEquals(0, database.listGames().size());
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void createUser(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void getUser(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void createGame(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void getGame(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void listGames(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void updateGame(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void createAuth(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void getAuth(DataAccess database) {
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("DataAccessImplementations")
+  void deleteAuthData(DataAccess database) {
+
   }
 
 }
